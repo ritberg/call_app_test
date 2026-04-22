7c300a045f395783229a987c1303ef3704a51273
ritberg <67046171+ritberg@users.noreply.github.com>
2026-04-22 18:33:43 +0200

    test

---
+++ server/server.js
@@ -0,0 +1,62 @@
+/*const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 3001 });
+
+let clients = new Set();
+
+wss.on("connection", (ws) => {
+  clients.add(ws);
+
+  ws.on("message", (message) => {
+    for (let client of clients) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    }
+  });
+
+  ws.on("close", () => {
+    clients.delete(ws);
+  });
+});
+
+console.log("WebSocket server running on ws://localhost:3001");*/
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3001 });
+
+const rooms = {};
+
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    const data = JSON.parse(msg);
+
+    const { type, room } = data;
+
+    if (!rooms[room]) {
+      rooms[room] = [];
+    }
+
+    if (!rooms[room].includes(ws)) {
+      rooms[room].push(ws);
+    }
+
+    ws.room = room;
+
+    // forward to other peer in same room
+    rooms[room].forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    const room = ws.room;
+    if (room && rooms[room]) {
+      rooms[room] = rooms[room].filter(c => c !== ws);
+    }
+  });
+});
+
+console.log("Server running on ws://localhost:3001");
